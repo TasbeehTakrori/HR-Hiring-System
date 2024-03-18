def55e6a8421f09dbac0131512100e4c61c96abc
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2024-03-18 17:44:06 +0200

    Create ValidationBehavior

---
+++ src/Core/HRHiringSystem.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using HRHiringSystem.Application.Abstractions.Messaging;
+using MediatR;
+
+namespace HRHiringSystem.Application.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : class, ICommand<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationTasks = _validators.Select(v => v.ValidateAsync(context, cancellationToken));
+        var validationResults = await Task.WhenAll(validationTasks);
+
+        var failures = validationResults
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null).ToList();
+
+        if (failures.Any())
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
+}
