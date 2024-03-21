ba2d6b40d5ca9b3d9e5dbc668957ea785ce7d8f8
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2024-03-21 17:53:46 +0200

    Add InlineAutoMoqDataAttribute

---
+++ test/HRHiringSystem.Application.Tests/Attributes/InlineAutoMoqDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRHiringSystem.Application.Tests.Attributes;
+internal class InlineAutoMoqDataAttribute : CompositeDataAttribute
+{
+    public InlineAutoMoqDataAttribute(params object[] values)
+        : base(new InlineDataAttribute(values), new AutoMoqDataAttribute())
+    {
+    }
+}
