847ab05f0e832a046b48fd94be84d41ccb8e83a9
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2024-05-27 15:41:07 +0300

    Create EmailSender

---
+++ src/Core/HRHiringSystem.Application/Features/EmailSender.cs
@@ -0,0 +1,54 @@
+﻿namespace HRHiringSystem.Application.Features;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+//TODO: namespace HRHiringSystem.Application.Features???;
+
+public interface IEmailSender
+{
+    Task SendEmailAsync(string email, string subject, string message);
+}
+
+public class EmailSender : IEmailSender
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailSender(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string message)
+    {
+        var smtpClient = new SmtpClient
+        {
+            Host = _emailSettings.Host,
+            Port = _emailSettings.Port,
+            EnableSsl = true,
+            Credentials = new NetworkCredential(_emailSettings.UserName, _emailSettings.Password)
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(_emailSettings.SenderEmail, _emailSettings.SenderName),
+            Subject = subject,
+            Body = message,
+            IsBodyHtml = true
+        };
+
+        mailMessage.To.Add(email);
+
+        await smtpClient.SendMailAsync(mailMessage);
+    }
+}
+
+public class EmailSettings
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string SenderName { get; set; }
+    public string SenderEmail { get; set; }
+}
